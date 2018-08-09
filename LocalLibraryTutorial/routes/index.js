9c9b941be87de8bf89e291681c5b50d0a712950b
nielchvo <nielchvo@gmail.com>
2018-08-10 00:01:53 +0700

    ! first commit

---
+++ LocalLibraryTutorial/routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'My Local Library'});
+});
+
+// TODO: do a MVC model
+/* Books page Routes */
+
+router.get('/books', function(req, res, next) {
+  res.render('list_book', {title: m_title});
+});
+
+router.get('/book/add', function(req, res, next) {
+  res.render('create_book', {title: 'Create book'});
+});
+
+
+/* Authors page Routes*/
+
+router.get('/authors', function(req, res, next) {
+  res.render('list_author', {title: m_title});
+});
+
+router.get('/author/add', function(req, res, next) {
+  res.render('create_author', {title: 'Create Author'});
+});
+
+
+/* Genres page Routes*/
+
+router.get('/genres', function(req, res, next) {
+  res.render('list_genre', {title: m_title});
+});
+
+router.get('/genre/add', function(req, res, next) {
+  res.render('create_genre', {title: 'create genre'});
+});
+
+
+/* Book Instance page Routes*/
+
+router.get('/bookinstance', function(req, res, next) {
+  res.render('list_bookinstance', {title: m_title});
+});
+
+router.get('/bookinstance/add', function(req, res, next) {
+  res.render('create_bookinstance', {title: 'create book instance'});
+});
+
+module.exports = router;
