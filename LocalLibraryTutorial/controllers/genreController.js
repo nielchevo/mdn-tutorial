34e626409507b2730dc5af12c85e990e46ce28b6
nielchevo <nielchvo@gmail.com>
2018-09-27 00:58:19 +0700

    add progress

--- LocalLibraryTutorial/controllers/genreController.js
+++ LocalLibraryTutorial/controllers/genreController.js
@@ -97,14 +97,69 @@ exports.genre_delete_post = function(req, res) {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = function(req, res, next) {
+    let genreId = req.params.id;
+
+    async.parallel({
+        // Async tasks
+        Genre: function(callback) {
+            db_genreModel.findById(genreId)
+                .exec(callback);
+        }
+    }, function(err, results) {
+        // Callback handler
+        if(err) { 
+            return next(err);
+        }
+
+        // Success. then render to view.
+        res.render('create_genre', {title: 'Update Genre',
+                                    errors: err,
+                                    genre: results.Genre
+        });
+    });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+    body('name', 'Name must not be empty!').isLength({min: 1}).trim(),
+    body('name', 'Name must Contain at least 3 character').isLength({min: 3}).trim(),
+    
+    sanitizeBody('name').trim().escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        
+        let genreId = req.params.id;
+
+        let update_genre = new db_genreModel({
+                                            name: req.body.name,
+                                            _id : genreId
+                            });
+        
+        if(!errors.isEmpty()) {
+            // Error handle 
+
+            res.render('create_genre', {title: 'Update Genre',
+                                        errors: errors.array(),
+                                        genre: genre,
+                                        _id: genreId 
+            });
+        } else {
+            db_genreModel.findByIdAndUpdate(genreId,
+                                            update_genre,
+                                            {},
+                                            function(err, updateResults) {
+
+                if(err) { 
+                    return next(err);
+                }
+                // Success. then update and redirect.
+                res.redirect(updateResults.url);
+            });
+        }
+    }
+];
 
 // Display detail page for a specific Genre.
 exports.genre_detail = function(req, res, next) {
